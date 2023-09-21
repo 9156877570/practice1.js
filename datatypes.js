ce3b1cf20add3488bd062943dfdc73dad958be24
Ganesh Ghode <116545495+9156877570@users.noreply.github.com>
2023-09-21 13:28:14 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,58 @@
+// 1.primitive datatype
+// ->Number
+
+// let age1=30
+// let age2=40
+// if(age1>age2){
+//     console.log(`age  ${age1} is greater`)
+// }
+// else{
+//     console.log(`age  ${age2} is greater`)
+// }
+
+
+
+// ->String
+
+// let name='ganesh'
+// let surname=' ghode'
+// let fullName=name+surname
+// console.log(fullName)
+// console.log(typeof fullName)
+
+
+
+// ->null
+
+// let gamename=null
+// console.log(gamename);
+
+
+// ->Boolean
+
+// let loggedIn=true
+// let loggedOut=false
+// console.log(loggedIn)
+// console.log(loggedOut)
+
+
+
+// ->undefined
+// let age;
+// console.log(age)
+
+
+// ->Symbol
+// let symbol=Symbol('addd')
+// console.log(symbol)
+
+
+
+
+// 2.Non_Primittive datatype
+
+// ->Array
+// let nums=[1,2,3,4,5,6]
+// let num2=[2,3,4,5,5,6,]
+// let num3=nums.concat(num2)
+// console.log(num3)
