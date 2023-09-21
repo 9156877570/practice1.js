7d540a9d035194c3da4cf2946fc8a152e050af9c
Ganesh Ghode <116545495+9156877570@users.noreply.github.com>
2023-09-21 19:28:44 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,26 @@
+// let arr1=[1,2,3,4,5,6,7,8,9]
+// let arr2=[1,2,3,4,[1,2,3,4,5,6,7,8,9],5,6,7,8,9]
+// let arr3=arr1.concat(arr2)
+// // console.log(arr3)
+// // let arrn=[...arr1,...arr2]
+// // console.log(arrn)
+// // console.log(arr2)
+// // let arrn1=arr2.flat(1)
+// // console.log(arrn1)
+// arr1.push(2,3,4)
+// console.log(arr1)
+// arr1.pop()
+// console.log(arr1)
+
+let arr1=[1,2,3,4,5,6,7,8,9]
+let arr2=[1,100,2,3,4,[1,2,3,4,5,6,7,8,9],5,6,7,8,9]
+let arr3=arr2.flat(Infinity)
+// console.log(arr3.sort())
+// console.log(arr3.shift())
+// console.log(arr3.shift())
+// console.log(arr3.sort())
+// console.log(arr3)
+// let arr4=arr3.slice(2,12)
+// console.log(arr4)
+// console.log(arr3.toLocaleString())
+// console.log(arr3.join('-'))
