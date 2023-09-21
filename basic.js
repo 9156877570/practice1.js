51f3eca740b362d097c32a8b737628052c42b651
Ganesh Ghode <116545495+9156877570@users.noreply.github.com>
2023-09-21 12:19:05 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,19 @@
+//  learning Javscript from basic 
+
+// 1.first video is based on basic concepts like variables,
+// 2.difference between const,let and var
+// 3.dont use var
+
+
+
+let name="ganesh"
+console.log(name)
+
+
+let age=20
+console.log(age)
+
+const accountId=1127373
+console.log(accountId)
+
+console.table([age,name,accountId])
