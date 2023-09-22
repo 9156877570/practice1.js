0da622a226fb418e915b285d48c40e490a92a1e5
Ganesh Ghode <116545495+9156877570@users.noreply.github.com>
2023-09-22 22:23:08 +0530

    Add files via upload

---
+++ javascript_practice.js
@@ -0,0 +1,72 @@
+// let name='ganesh'
+// const gender="male";
+// let surname="ghode"
+// let detail=`My name is ${name} and Surname is ${surname} and gender is ${gender}`
+// console.log(detail)
+// let age=21;
+// if(age>18){
+//     console.log("you can vote")
+// }else{
+//     console.log("you can not vote")
+// }
+
+
+// In javascript datatype
+// @primitive
+// 1.Number
+// 2.boolean
+// 3.string
+// 4.null
+// 5.undefined
+// 6.bigint
+// 7.symbol
+
+
+// @non-primitive
+// 1.Array
+// 2.objects
+
+
+//in javascript memory type
+// @Stack memory
+// @heap memory
+
+
+//conversion of dattypes
+//to number 
+//to string
+//to boolean
+
+
+// function addTwoNumber(num1,num2){
+    
+//     let sum=num1+num2
+
+//     return (sum)
+
+// }
+// let a=addTwoNumber(3,5)
+// console.log(a)
+
+// ((name)=>{
+//     console.log(`hello world,${name}`)
+// })("ganesh")
+
+// details=()=>{
+//     console.log('hellllo')
+// }
+// details()
+// let sym=Symbol('abccc');
+
+// let object={
+//     name:"ganesh",
+//     age:21,
+//     [sym]:Symbol('abccc'),
+//     city:'ulhasnagar',
+//     degree:"B-tech"
+// }
+// console.log(object[sym])
+// console.log(object["name"])
+// for(let i=0;i<=object.lenght;i++){
+//     console.log(i)
+// }
